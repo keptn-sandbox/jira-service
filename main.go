3418e37f1e3ce42a6de405bd32c15cdf457a5411
akirasoft <akirasoft@gmail.com>
2019-08-21 11:52:07 -0500

    initial commit THIS DOESNT WORK YET

--- main.go
+++ main.go
@@ -1,63 +1,56 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"io"
 	"io/ioutil"
 	"log"
-	"net/http"
 	"os"
 	"strconv"
 	"strings"
 
 	"github.com/andygrunwald/go-jira"
+	cloudevents "github.com/cloudevents/sdk-go"
 )
 
-type keptnEvent struct {
-	Specversion     string `json:"specversion"`
-	Type            string `json:"type"`
-	Source          string `json:"source"`
-	ID              string `json:"id"`
-	Time            string `json:"time"`
-	Datacontenttype string `json:"datacontenttype"`
-	Shkeptncontext  string `json:"shkeptncontext"`
-	Data            struct {
-		Githuborg          string `json:"githuborg"`
-		Project            string `json:"project"`
-		Teststrategy       string `json:"teststrategy"`
-		Deploymentstrategy string `json:"deploymentstrategy"`
-		Stage              string `json:"stage"`
-		Service            string `json:"service"`
-		Image              string `json:"image"`
-		Tag                string `json:"tag"`
-		Evaluationpassed   bool   `json:"evaluationpassed"`
-		Evaluationdetails  struct {
-			Options struct {
-				TimeStart int `json:"timeStart"`
-				TimeEnd   int `json:"timeEnd"`
-			} `json:"options"`
-			TotalScore int `json:"totalScore"`
-			Objectives struct {
-				Pass    int `json:"pass"`
-				Warning int `json:"warning"`
-			} `json:"objectives"`
-			// Data coming back from Prometheus sources is not strongly typed
-			// especially within indicatorResults
-			IndicatorResults []struct {
-				ID         string `json:"id"`
-				Violations []struct {
-					Value interface{} `json:"value"`
-					// we need to  take the key as raw json and parse it later
-					Key       json.RawMessage `json:"key"`
-					Breach    string          `json:"breach"`
-					Threshold interface{}     `json:"threshold"`
-				} `json:"violations"`
-				Score int `json:"score"`
-			} `json:"indicatorResults"`
-			Result string `json:"result"`
-		} `json:"evaluationdetails"`
-	} `json:"data"`
+// EvaluationDoneEvent payload for evaluation done event, see keptn spec
+type EvaluationDoneEvent struct {
+	Githuborg          string `json:"githuborg"`
+	Project            string `json:"project"`
+	Teststrategy       string `json:"teststrategy"`
+	Deploymentstrategy string `json:"deploymentstrategy"`
+	Stage              string `json:"stage"`
+	Service            string `json:"service"`
+	Image              string `json:"image"`
+	Tag                string `json:"tag"`
+	Evaluationpassed   bool   `json:"evaluationpassed"`
+	Evaluationdetails  struct {
+		Options struct {
+			TimeStart int `json:"timeStart"`
+			TimeEnd   int `json:"timeEnd"`
+		} `json:"options"`
+		TotalScore int `json:"totalScore"`
+		Objectives struct {
+			Pass    int `json:"pass"`
+			Warning int `json:"warning"`
+		} `json:"objectives"`
+		// Data coming back from Prometheus sources is not strongly typed
+		// especially within indicatorResults
+		IndicatorResults []struct {
+			ID         string `json:"id"`
+			Violations []struct {
+				Value interface{} `json:"value"`
+				// we need to  take the key as raw json and parse it later
+				Key       json.RawMessage `json:"key"`
+				Breach    string          `json:"breach"`
+				Threshold interface{}     `json:"threshold"`
+			} `json:"violations"`
+			Score int `json:"score"`
+		} `json:"indicatorResults"`
+		Result string `json:"result"`
+	} `json:"evaluationdetails"`
 }
 
 // PrometheusKey is a json object containing job and an instance, we will use instance as it is more verbose
@@ -83,16 +76,16 @@ func Logging(infoLogger io.Writer, errorLogger io.Writer) {
 	errorLog = log.New(errorLogger, "ERROR: ", log.Ldate|log.Ltime|log.Lshortfile)
 }
 
-//keptnHandler : receives keptn events via http and sets UFO LEDs based on payload
-func keptnHandler(w http.ResponseWriter, r *http.Request) {
-	decoder := json.NewDecoder(r.Body)
-	var event keptnEvent
-	err := decoder.Decode(&event)
-	if err != nil {
-		fmt.Println("Error while parsing JSON payload: " + err.Error())
+//keptnHandler : receives keptn events via http and creates JIRA tickets for failed evaluations
+func keptnHandler(event cloudevents.Event) {
+	var shkeptncontext string
+	event.Context.ExtensionAs("shkeptncontext", &shkeptncontext)
+
+	data := &EvaluationDoneEvent{}
+	if err := event.DataAs(data); err != nil {
+		fmt.Printf("Got Data Error: %s", err.Error())
 		return
 	}
-
 	jiraHostname = os.Getenv("JIRA_HOSTNAME")
 	if jiraHostname == "" {
 		errorLog.Println("No JIRA hostname defined")
@@ -111,19 +104,19 @@ func keptnHandler(w http.ResponseWriter, r *http.Request) {
 
 	jiraProject = os.Getenv("JIRA_PROJECT")
 	if jiraProject == "" {
-		jiraProject = strings.ToUpper(event.Data.Project)
+		jiraProject = strings.ToUpper(data.Project)
 	}
 
-	if event.Type == "sh.keptn.events.evaluation-done" {
-		if event.Data.Evaluationpassed != true {
+	if event.Type() == "sh.keptn.events.evaluation-done" {
+		if data.Evaluationpassed != true {
 			infoLog.Println("Trying to talk to JIRA at " + jiraHostname)
 			infoLog.Println("using JIRA project " + jiraProject)
-			postJIRAIssue(jiraHostname, event)
+			postJIRAIssue(jiraHostname, *data)
 		}
 	}
 }
 
-func postJIRAIssue(jiraHostname string, event keptnEvent) {
+func postJIRAIssue(jiraHostname string, data EvaluationDoneEvent) {
 	var strViolationsValue string
 	var strKey string
 	var strValThreshold string
@@ -135,56 +128,56 @@ func postJIRAIssue(jiraHostname string, event keptnEvent) {
 	//	fmt.Println("URL to be used: " + url)
 
 	// iterating through the contents of IndicatorResults so they can be sent to JIRA
-	for i := 0; i < len(event.Data.Evaluationdetails.IndicatorResults); i++ {
-		for v := 0; v < len(event.Data.Evaluationdetails.IndicatorResults[i].Violations); v++ {
+	for i := 0; i < len(data.Evaluationdetails.IndicatorResults); i++ {
+		for v := 0; v < len(data.Evaluationdetails.IndicatorResults[i].Violations); v++ {
 
-			valDouble, ok := event.Data.Evaluationdetails.IndicatorResults[i].Violations[v].Value.(float64)
+			valDouble, ok := data.Evaluationdetails.IndicatorResults[i].Violations[v].Value.(float64)
 			if ok {
 				strViolationsValue = fmt.Sprintf("%f", valDouble)
 			}
-			valBoolean, ok := event.Data.Evaluationdetails.IndicatorResults[i].Violations[v].Value.(bool)
+			valBoolean, ok := data.Evaluationdetails.IndicatorResults[i].Violations[v].Value.(bool)
 			if ok {
 				strViolationsValue = fmt.Sprintf("%t", valBoolean)
 			}
-			valString, ok := event.Data.Evaluationdetails.IndicatorResults[i].Violations[v].Value.(string)
+			valString, ok := data.Evaluationdetails.IndicatorResults[i].Violations[v].Value.(string)
 			if ok {
 				strViolationsValue = valString
 			}
 			// threshold might not exist and should be a float64, if it is a string this will say it isn't there...
-			valThreshold, ok := event.Data.Evaluationdetails.IndicatorResults[i].Violations[v].Threshold.(float64)
+			valThreshold, ok := data.Evaluationdetails.IndicatorResults[i].Violations[v].Threshold.(float64)
 			if ok {
 				strValThreshold = strconv.FormatFloat(valThreshold, 'f', -1, 64)
 			} else {
 				strValThreshold = "No Threshold in Pitometer response"
 			}
 
-			if err := json.Unmarshal(event.Data.Evaluationdetails.IndicatorResults[i].Violations[v].Key, &keyDT); err == nil {
+			if err := json.Unmarshal(data.Evaluationdetails.IndicatorResults[i].Violations[v].Key, &keyDT); err == nil {
 				strKey = keyDT
 			}
 			// Prometheus Key is an object containing job and an instance, we will use instance as it is more verbose
 
-			if err := json.Unmarshal(event.Data.Evaluationdetails.IndicatorResults[i].Violations[v].Key, &keyProm); err == nil {
+			if err := json.Unmarshal(data.Evaluationdetails.IndicatorResults[i].Violations[v].Key, &keyProm); err == nil {
 				strKey = keyProm.Instance
 			}
 			indicatorValues = indicatorValues +
-				"\nIndicator ID: " + event.Data.Evaluationdetails.IndicatorResults[i].ID +
+				"\nIndicator ID: " + data.Evaluationdetails.IndicatorResults[i].ID +
 				"\nIndicator Key: " + strKey +
 				"\nIndicator Value: " + strViolationsValue +
 				"\nIndicator Threshold: " + strValThreshold +
-				"\nIndicator Breach: " + event.Data.Evaluationdetails.IndicatorResults[i].Violations[v].Breach
+				"\nIndicator Breach: " + data.Evaluationdetails.IndicatorResults[i].Violations[v].Breach
 		}
 	}
 
 	jiraIssue := "Keptn test evaluation failed, build was not deployed" +
-		"\nshkeptncontext: " + event.Shkeptncontext +
-		"\nFailed stage: " + event.Data.Stage +
-		"\nFailed service: " + event.Data.Service +
-		"\nGithuborg: " + event.Data.Githuborg +
-		"\nTotal score: " + strconv.Itoa(event.Data.Evaluationdetails.TotalScore) +
-		"\nPass Threshold: " + strconv.Itoa(event.Data.Evaluationdetails.Objectives.Pass) +
-		"\nWarning Threshold: " + strconv.Itoa(event.Data.Evaluationdetails.Objectives.Warning) +
+		//"\nshkeptncontext: " + event.Shkeptncontext +
+		"\nFailed stage: " + data.Stage +
+		"\nFailed service: " + data.Service +
+		"\nGithuborg: " + data.Githuborg +
+		"\nTotal score: " + strconv.Itoa(data.Evaluationdetails.TotalScore) +
+		"\nPass Threshold: " + strconv.Itoa(data.Evaluationdetails.Objectives.Pass) +
+		"\nWarning Threshold: " + strconv.Itoa(data.Evaluationdetails.Objectives.Warning) +
 		indicatorValues +
-		"\nOverall Result: " + event.Data.Evaluationdetails.Result
+		"\nOverall Result: " + data.Evaluationdetails.Result
 
 	tp := jira.BasicAuthTransport{
 		Username: jiraUsername,
@@ -232,13 +225,27 @@ func postJIRAIssue(jiraHostname string, event keptnEvent) {
 func main() {
 	Logging(os.Stdout, os.Stderr)
 
-	http.HandleFunc("/", keptnHandler)
-
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
+	Port, err := strconv.Atoi(os.Getenv("PORT"))
+	if Port == 0 {
+		Port = 8080
+	}
+	Path := os.Getenv("PATH")
+	if Path == "" {
+		Path = "/"
+	}
+	t, err := cloudevents.NewHTTPTransport(
+		cloudevents.WithPort(Port),
+		cloudevents.WithPath(Path),
+	)
+	if err != nil {
+		log.Fatalf("failed to create transport, %v", err)
 	}
 
 	log.Print("JIRA service started.")
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
+
+	c, err := cloudevents.NewClient(t)
+	if err != nil {
+		log.Fatalf("failed to create client, %v", err)
+	}
+	log.Fatal(c.StartReceiver(context.Background(), keptnHandler))
 }
